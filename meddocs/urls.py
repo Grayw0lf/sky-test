31a0c68deb5c847a059a9162402fd994b1c4571d
Graywolf <akosheev@rambler.ru>
2021-07-26 22:40:15 +0300

    Добавлены, изменены фильтры, добавлены параметры сваггера.

--- meddocs/urls.py
+++ meddocs/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PatientViewSet, TreatmentViewSet
+from .views import PatientViewSet, TreatmentViewSet, MedicalDocViewSet
 
 
 router = routers.DefaultRouter()
 router.register('patients', PatientViewSet)
 router.register('treatments', TreatmentViewSet)
+router.register('medical-docs', MedicalDocViewSet)
 
 
 urlpatterns = [
--- meddocs/views.py
+++ meddocs/views.py
@@ -1,6 +1,14 @@
 from rest_framework import viewsets, permissions
 from .models import Patient, Treatment, MedicalDoc, BodyDoc
 from .serializers import PatientSerializer, TreatmentSerializer, MedicalDocSerializer
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
+
+patient_param = openapi.Parameter('patient', openapi.IN_QUERY, description="Filter patient",
+                                  type=openapi.TYPE_STRING)
+treatment_param = openapi.Parameter('treatment', openapi.IN_QUERY, description="Filter treatment",
+                                    type=openapi.TYPE_INTEGER)
 
 
 class PatientViewSet(viewsets.ModelViewSet):
@@ -8,21 +16,41 @@ class PatientViewSet(viewsets.ModelViewSet):
     serializer_class = PatientSerializer
     permission_classes = [permissions.AllowAny]
 
-    def get_queryset(self):
-        queryset = self.queryset
-        filter_fio = self.request.query_params.get('fio')
-        if filter_fio:
-            queryset = queryset.filter(fio__contains=filter_fio)
-        return queryset
-
 
 class MedicalDocViewSet(viewsets.ModelViewSet):
     queryset = MedicalDoc.objects.all()
     serializer_class = MedicalDocSerializer
     permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        queryset = self.queryset
+        filter_patient = self.request.query_params.get('patient')
+        fileter_treatment = self.request.query_params.get('treatment')
+        if filter_patient:
+            queryset = queryset.filter(patient__fio__icontains=filter_patient)
+        if fileter_treatment:
+            queryset = queryset.filter(treatment=fileter_treatment)
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=[patient_param, treatment_param])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
 
 class TreatmentViewSet(viewsets.ModelViewSet):
     queryset = Treatment.objects.all()
     serializer_class = TreatmentSerializer
     permission_classes = [permissions.AllowAny]
+    http_method_names = ['get', 'post']
+
+    def get_queryset(self):
+        queryset = self.queryset
+        filter_patient = self.request.query_params.get('patient')
+        if filter_patient:
+            queryset = queryset.filter(patient__fio__icontains=filter_patient)
+        return queryset
+
+    @swagger_auto_schema(manual_parameters=[patient_param])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
